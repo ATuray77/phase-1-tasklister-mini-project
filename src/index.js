19e0e9a8d508c3c914e2f13e6da64dc3a8ef8165
Aruna Turay <tura_arun@yahoo.com>
2023-04-12 16:21:34 -0400

    form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //console.log('js loading...')
+  let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     buildTaskList(e.target['new-task-description'].value)
+     form.reset()
+  }) 
 });
+
+
+//function to build to Task Lister elements
+function buildTaskList(todo){
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.textContent = 'X'
+btn.addEventListener('click', handleDelete)
+li.textContent = `${todo} `
+li.appendChild(btn)
+document.querySelector('#tasks').appendChild(li)
+
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
